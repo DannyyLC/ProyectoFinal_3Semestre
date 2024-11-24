46a86ac970a6b8a134fac5addea633949b270cc7
Danny Limon <cervantesdaniellimon50m@gmail.com>
2024-11-24 12:14:57 -0600

    Header Images

--- ProyectoProgramacion/ProyectoProgramacion/Views/UserMainForm.Designer.cs
+++ ProyectoProgramacion/ProyectoProgramacion/Views/UserMainForm.Designer.cs
@@ -21,74 +21,177 @@ namespace ProyectoProgramacion.Views
         {
             PanelHeader = new Panel();
             panelSearchBox = new Panel();
-            logoPictureBox = new PictureBox();
             navBar = new Panel();
-            navBarFlowLayout = new FlowLayoutPanel();
+            navBarPanel = new FlowLayoutPanel();
+            AccountPanel = new Panel();
+            CartPanel = new Panel();
+            CartPicture = new PictureBox();
+            AccountPicture = new PictureBox();
+            LogoPanel = new Panel();
+            LogoPicture = new PictureBox();
+            loupePanel = new Panel();
+            LoupePicture = new PictureBox();
             PanelHeader.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)logoPictureBox).BeginInit();
+            panelSearchBox.SuspendLayout();
             navBar.SuspendLayout();
+            AccountPanel.SuspendLayout();
+            CartPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)CartPicture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)AccountPicture).BeginInit();
+            LogoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)LogoPicture).BeginInit();
+            loupePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)LoupePicture).BeginInit();
             SuspendLayout();
             // 
             // PanelHeader
             // 
             PanelHeader.BackColor = Color.White;
+            PanelHeader.Controls.Add(LogoPanel);
+            PanelHeader.Controls.Add(CartPanel);
+            PanelHeader.Controls.Add(AccountPanel);
             PanelHeader.Controls.Add(panelSearchBox);
-            PanelHeader.Controls.Add(logoPictureBox);
             PanelHeader.Dock = DockStyle.Top;
             PanelHeader.Location = new Point(0, 0);
             PanelHeader.Name = "PanelHeader";
-            PanelHeader.Size = new Size(1144, 114);
+            PanelHeader.Size = new Size(1312, 114);
             PanelHeader.TabIndex = 0;
             // 
             // panelSearchBox
             // 
-            panelSearchBox.Location = new Point(294, 10);
+            panelSearchBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panelSearchBox.BackColor = Color.Gainsboro;
+            panelSearchBox.Controls.Add(loupePanel);
+            panelSearchBox.Location = new Point(298, 10);
             panelSearchBox.Name = "panelSearchBox";
-            panelSearchBox.Size = new Size(689, 87);
+            panelSearchBox.Size = new Size(751, 87);
             panelSearchBox.TabIndex = 1;
-            // 
-            // logoPictureBox
-            // 
-            logoPictureBox.BackColor = Color.Transparent;
-            logoPictureBox.Location = new Point(10, 10);
-            logoPictureBox.Name = "logoPictureBox";
-            logoPictureBox.Size = new Size(263, 87);
-            logoPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            logoPictureBox.TabIndex = 0;
-            logoPictureBox.TabStop = false;
             // 
             // navBar
             // 
             navBar.BackColor = Color.WhiteSmoke;
-            navBar.Controls.Add(navBarFlowLayout);
+            navBar.Controls.Add(navBarPanel);
             navBar.Dock = DockStyle.Top;
             navBar.Location = new Point(0, 114);
             navBar.Name = "navBar";
-            navBar.Size = new Size(1144, 50);
+            navBar.Size = new Size(1312, 50);
             navBar.TabIndex = 1;
             // 
-            // navBarFlowLayout
+            // navBarPanel
             // 
-            navBarFlowLayout.Dock = DockStyle.Fill;
-            navBarFlowLayout.Location = new Point(0, 0);
-            navBarFlowLayout.Name = "navBarFlowLayout";
-            navBarFlowLayout.Padding = new Padding(20, 10, 0, 0);
-            navBarFlowLayout.Size = new Size(1144, 50);
-            navBarFlowLayout.TabIndex = 0;
+            navBarPanel.BackColor = Color.Gainsboro;
+            navBarPanel.Dock = DockStyle.Fill;
+            navBarPanel.Location = new Point(0, 0);
+            navBarPanel.Name = "navBarPanel";
+            navBarPanel.Padding = new Padding(20, 10, 0, 0);
+            navBarPanel.Size = new Size(1312, 50);
+            navBarPanel.TabIndex = 0;
+            // 
+            // AccountPanel
+            // 
+            AccountPanel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            AccountPanel.BackColor = Color.Transparent;
+            AccountPanel.Controls.Add(AccountPicture);
+            AccountPanel.Location = new Point(1181, 19);
+            AccountPanel.Name = "AccountPanel";
+            AccountPanel.Size = new Size(100, 70);
+            AccountPanel.TabIndex = 2;
+            // 
+            // CartPanel
+            // 
+            CartPanel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            CartPanel.BackColor = Color.Transparent;
+            CartPanel.Controls.Add(CartPicture);
+            CartPanel.Location = new Point(1066, 19);
+            CartPanel.Name = "CartPanel";
+            CartPanel.Size = new Size(100, 70);
+            CartPanel.TabIndex = 3;
+            // 
+            // CartPicture
+            // 
+            CartPicture.Dock = DockStyle.Fill;
+            CartPicture.Image = Properties.Resources.shopping_cart;
+            CartPicture.Location = new Point(0, 0);
+            CartPicture.Name = "CartPicture";
+            CartPicture.Padding = new Padding(2);
+            CartPicture.Size = new Size(100, 70);
+            CartPicture.SizeMode = PictureBoxSizeMode.Zoom;
+            CartPicture.TabIndex = 0;
+            CartPicture.TabStop = false;
+            // 
+            // AccountPicture
+            // 
+            AccountPicture.Dock = DockStyle.Fill;
+            AccountPicture.Image = Properties.Resources.user;
+            AccountPicture.Location = new Point(0, 0);
+            AccountPicture.Name = "AccountPicture";
+            AccountPicture.Size = new Size(100, 70);
+            AccountPicture.SizeMode = PictureBoxSizeMode.Zoom;
+            AccountPicture.TabIndex = 0;
+            AccountPicture.TabStop = false;
+            // 
+            // LogoPanel
+            // 
+            LogoPanel.BackColor = Color.Transparent;
+            LogoPanel.Controls.Add(LogoPicture);
+            LogoPanel.Location = new Point(12, 10);
+            LogoPanel.Name = "LogoPanel";
+            LogoPanel.Size = new Size(267, 87);
+            LogoPanel.TabIndex = 4;
+            // 
+            // LogoPicture
+            // 
+            LogoPicture.Dock = DockStyle.Fill;
+            LogoPicture.Image = Properties.Resources.Kicks__600_x_100_;
+            LogoPicture.Location = new Point(0, 0);
+            LogoPicture.Name = "LogoPicture";
+            LogoPicture.Size = new Size(267, 87);
+            LogoPicture.SizeMode = PictureBoxSizeMode.Zoom;
+            LogoPicture.TabIndex = 0;
+            LogoPicture.TabStop = false;
+            // 
+            // loupePanel
+            // 
+            loupePanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            loupePanel.BackColor = Color.Transparent;
+            loupePanel.Controls.Add(LoupePicture);
+            loupePanel.Location = new Point(16, 9);
+            loupePanel.Name = "loupePanel";
+            loupePanel.Size = new Size(94, 68);
+            loupePanel.TabIndex = 0;
+            // 
+            // LoupePicture
+            // 
+            LoupePicture.Dock = DockStyle.Fill;
+            LoupePicture.Image = Properties.Resources.loupe;
+            LoupePicture.Location = new Point(0, 0);
+            LoupePicture.Name = "LoupePicture";
+            LoupePicture.Size = new Size(94, 68);
+            LoupePicture.SizeMode = PictureBoxSizeMode.Zoom;
+            LoupePicture.TabIndex = 0;
+            LoupePicture.TabStop = false;
             // 
             // UserMainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1144, 599);
+            ClientSize = new Size(1312, 640);
             Controls.Add(navBar);
             Controls.Add(PanelHeader);
             Name = "UserMainForm";
             Text = "VistaUsuario";
             PanelHeader.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)logoPictureBox).EndInit();
+            panelSearchBox.ResumeLayout(false);
             navBar.ResumeLayout(false);
+            AccountPanel.ResumeLayout(false);
+            CartPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)CartPicture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)AccountPicture).EndInit();
+            LogoPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)LogoPicture).EndInit();
+            loupePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)LoupePicture).EndInit();
             ResumeLayout(false);
         }
 
@@ -96,9 +199,16 @@ namespace ProyectoProgramacion.Views
 
         private Panel PanelHeader;
         private Panel navBar;
-        private FlowLayoutPanel navBarFlowLayout; // Contenedor para botones de navegación
+        private FlowLayoutPanel navBarPanel; // Contenedor para botones de navegación
         private Panel panelSearchBox;
-        private PictureBox logoPictureBox;
+        private Panel CartPanel;
+        private Panel AccountPanel;
+        private PictureBox CartPicture;
+        private PictureBox AccountPicture;
+        private Panel LogoPanel;
+        private PictureBox LogoPicture;
+        private Panel loupePanel;
+        private PictureBox LoupePicture;
     }
 }
 
