5b030de5088de795a63afca77d36749d19f9cf97
Juan Antonio <jaalbacardona@gmail.com>
2024-11-29 20:44:00 -0600

    Programa basico checa usuario y contraseña

---
+++ ProyectoProgramacion/ProyectoProgramacion/Models/Login1.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+public class login
+{
+    private static string usuario = "Juan";
+    private static string contra = "hola";
+    public bool verifica(string username, string password)
+    {
+        if (username == null || password == null)
+        {
+            throw new ArgumentNullException("Campos no valida, favor de insertar lo que se indica");
+        }
+        else if (username == usuario && password == contra)
+        {
+            return true;
+        }
+        else { return false; }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        login persona1 = new login();
+        string username, password;
+        bool band = false;
+
+        while (band == false)
+        {
+            Console.WriteLine("Inserte su nombre de usuario: ");
+            username = Console.ReadLine();
+            Console.WriteLine("Inserte su Contraseña: ");
+            password = Console.ReadLine();
+
+            band = persona1.verifica(username, password);
+
+            if (band == false)
+            {
+                Console.WriteLine("Usuario o contraseña incorrecto\n");
+            }
+        }
+    }
+}
